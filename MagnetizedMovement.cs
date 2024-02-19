1c4bf2c1229ac8ebb5668bba0066088c3788376a
MrSoubi <122730595+MrSoubi@users.noreply.github.com>
2024-02-19 22:05:40 +0100

    Update MagnetizedMovement.cs

--- MagnetizedMovement.cs
+++ MagnetizedMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Attach this script to an object with a trigger collider to make it follow any GameObject containing a RigidBody with the tag defined in the variable "m_tag".
+// Movement will start when a new target enters the trigger and will always finish
 // If m_tag is left empty, it will bypass the verification and accept any GameObject
 // The movement will take m_animationTime seconds and follow the animation curve m_curve.
 
